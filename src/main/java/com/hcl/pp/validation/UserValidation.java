a588ba4a3d75dd2e0c6d9f23f3c95785f31e25cf
siva3060 <siva3060@gmail.com>
2019-04-30 14:28:06 +0530

    created validate

--- src/main/java/com/hcl/pp/validation/UserValidation.java
+++ src/main/java/com/hcl/pp/validation/UserValidation.java
@@ -1,4 +1,24 @@
 
 
+@service
 public class UserValidate{
+
+	@Autowired
+	UserRepository userRepository;
+
+
+
+	//validate the user
+	//1. weather the user exist or not
+	public void doesUserExist(@RequestBody UserValidationRequest userValidationRequest){
+
+		//get record from the database
+		User user = userRepository.findByUserId(userValidationRequest.getUserName());
+
+		if(user 
+
+		
+		
+		//end of the method doesUserExist
+	}
 }
