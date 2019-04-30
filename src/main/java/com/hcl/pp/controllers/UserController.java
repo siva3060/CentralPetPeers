8c9f9f3185d29c6464b4d4f88c186fb51bc86cb9
siva3060 <siva3060@gmail.com>
2019-04-30 15:22:32 +0530

    method my pets created

--- src/main/java/com/hcl/pp/controllers/UserController.java
+++ src/main/java/com/hcl/pp/controllers/UserController.java
@@ -1,5 +1,21 @@
 package com.hcl.pp.controllers;
 
+
+
+
 public class UserController {
+
+
+	@Autowired
+	PetRepository petRepository;
+
+
+	//get all pets
+	public List getAllPetsOfUser(@PathId int userId){
+
+
+		return petRepository.findAllPets();
+
+	}
 
 }
