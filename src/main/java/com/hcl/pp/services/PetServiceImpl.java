2a2463b32fa8a575889caa1181cfcedbe930ea6c
User1 <User1@LP-CNU3099WJB.HCLT.CORP.HCL.IN>
2019-04-30 14:59:38 +0530

    change

--- src/main/java/com/hcl/pp/services/PetServiceImpl.java
+++ src/main/java/com/hcl/pp/services/PetServiceImpl.java
@@ -1,5 +1,19 @@
 package com.hcl.pp.services;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.hcl.pp.Models.Pet;
+import com.hcl.pp.dtos.PetReqDTO;
+import com.hcl.pp.Models.*;
 
-public class PetServiceImpl {
+public class PetServiceImpl implements PetService {
+@Autowired
 
-}
+	@Override
+	public Pet addPet(PetReqDTO petReqDto) {
+				
+	
+		}
+
+	}
+
+
