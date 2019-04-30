7100e5ee691ca26b250416a1054cdaf18b1d4400
siva3060 <siva3060@gmail.com>
2019-04-30 15:07:13 +0530

    User validation sucessfull

--- src/main/java/com/hcl/pp/validation/UserValidation.java
+++ src/main/java/com/hcl/pp/validation/UserValidation.java
@@ -10,15 +10,36 @@ public class UserValidate{
 
 	//validate the user
 	//1. weather the user exist or not
-	public void doesUserExist(@RequestBody UserValidationRequest userValidationRequest){
+	public boolean  doesUserExist(string userName){
 
 		//get record from the database
-		User user = userRepository.findByUserId(userValidationRequest.getUserName());
+		User user = userRepository.findUserByName(userValidationRequest.getUserName());
 
-		if(user 
-
-		
+		if(user == Null){
+			return false;	
+		}else{
+			return true;
+		}
 		
 		//end of the method doesUserExist
+	}
+
+
+	//validate user and password
+	public boolean isValidUser(@RequestBody UserValidationRequest userValidationRequest){
+
+		//make sure user is there
+		//if user exist match password
+		//and return true
+
+		User validateUser = userRepository.findUserByName(userValidateionRequest);
+		if(doesUserExist(userValidationRequest.getUserName())){
+		
+			if(userValidationRequest.getUserPassword() == validateUser.getUserPassword()){
+				return true;
+			}else{
+				return false;
+			}
+		}
 	}
 }
