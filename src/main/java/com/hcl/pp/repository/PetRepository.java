0be85d3a886df48f9c0d678a54dd9d1c6bcd7a0c
siva3060 <siva3060@gmail.com>
2019-04-30 16:14:29 +0530

    added necessry file

--- src/main/java/com/hcl/pp/repository/PetRepository.java
+++ src/main/java/com/hcl/pp/repository/PetRepository.java
@@ -6,4 +6,7 @@ import com.hcl.pp.Models.Pet;
 
 public interface PetRepository extends JpaRepository<Pet,Integer> {
 
+
+	@Query(value = "select * from pet where userId = petOwnerId",nativeQuery = true)
+	List<Pet> findAllPets(Integer userId);
 }
